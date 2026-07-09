9644c3a26448981950bcede7ac5c34148e84c184
cat30to <.@>
2026-07-09 17:10:09 +0330

    i created a game to play with system v1

---
+++ main.py
@@ -0,0 +1,22 @@
+# guess number game
+
+from random import randint
+
+def computer_guess():
+    return randint(1,50)
+
+computer_num = computer_guess()
+running = True
+while running:
+    user = int(input("enter you'r number : "))
+    if user > computer_num:
+        print("you'r number bigger than me\n")
+    elif user < computer_num:
+        print("my number bigger than you\n")
+    else:
+        print('you win')
+        print(f'my number was {computer_num}')
+        isRunning = input('do you want to continue?[Y/n]').lower()
+        if isRunning == 'N' or isRunning == 'n':
+            running = False
+    
